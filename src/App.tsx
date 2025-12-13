13d1305b25b3d5b06f791c33451fe4168b463d29
egoron-dev <goronefren@gmail.com>
2025-12-13 22:33:52 +0800

    created sections and added to app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import About from "./sections/About";
+import Contacts from "./sections/Contacts";
+import Experiences from "./sections/Experiences";
+import Hero from "./sections/Hero";
+import Projects from "./sections/Projects";
+import Testimonials from "./sections/Testimonials";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <main className="p-6">
-        <h1>Testing</h1>
+      <main className="container mx-auto px-6 lg:px-10">
+       <Hero />
+       <About />
+       <Experiences />
+       <Projects />
+       <Testimonials />
+       <Contacts />
       </main>
+      <Footer />
     </>
   );
 }
