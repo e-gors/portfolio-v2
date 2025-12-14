1ecb9f266057b0aaaf61296d96098c2d83ae106a
egoron-dev <goronefren@gmail.com>
2025-12-14 18:11:49 +0800

    finalized experience section contents and styles

--- src/sections/Experiences.tsx
+++ src/sections/Experiences.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+// Timeline.jsx
+import React from "react";
+import { TimelineItem } from "@components/TimelineItem";
+import { workExperience } from "@constants/constants";
+import type { ExperienceItem } from "@types/types";
+import { education } from "@constants/constants";
+import type { EducationProps } from "@types/types";
+import GradientSphere from "@components/GradientSphere";
+import TitleHeader from "../components/TitleHeader";
 
-export default function Experiences() {
+const Experiences = () => {
   return (
-    <div id='experiences'>Experiences</div>
-  )
-}
+    <section className="w-screen bg-primary relative mx-auto px-4 md:px-10 lg:px-20 py-12 gap-8 md:gap-12 z-1">
+      <TitleHeader title="My Experiences" number="02" />
+      <div className="flex flex-wrap flex-col md:flex-row space-y-16 gap:5 md:gap-10">
+        {/* Work Experience */}
+        <div className="flex-1">
+          <h2 className="text-2xl font-bold mb-6">Work Experience</h2>
+          <div className="relative">
+            {workExperience.map((item: ExperienceItem, index: number) => (
+              <TimelineItem
+                key={index}
+                title={item.title}
+                subtitle={item.company}
+                date={item.date}
+                description={item.description}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* Education */}
+        <div className="flex-1">
+          <h2 className="text-2xl font-bold mb-6">Education</h2>
+          <div className="relative">
+            {education.map((item: EducationProps, index: number) => (
+              <TimelineItem
+                key={index}
+                title={item.degree}
+                subtitle={item.school}
+                date={item.date}
+                description={item.description}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      {/* Gradient Spheres */}
+      <GradientSphere firstSphere="sphere-1" secondSphere="sphere-4" />
+    </section>
+  );
+};
+
+export default Experiences;
