b1df699b84a86e19f7341914a927b010e0c566d0
egoron-dev <goronefren@gmail.com>
2025-12-14 16:52:00 +0800

    created for constant data

---
+++ src/constants/constants.ts
@@ -0,0 +1,104 @@
+import AWSIcon from "@icons/stacks/AWSIcon";
+import BootStrapIcon from "@icons/stacks/BootStrapIcon";
+import CSSIcon from "@icons/stacks/CSSIcon";
+import FirebaseIcon from "@icons/stacks/FirebaseIcon";
+import GithubIcon from "@icons/stacks/GithubIcon";
+import GitIcon from "@icons/stacks/GitIcon";
+import HTMLIcon from "@icons/stacks/HTMLIcon";
+import JavaScriptIcon from "@icons/stacks/JavaScriptIcon";
+import LaravelIcon from "@icons/stacks/LaravelIcon";
+import MailgunIcon from "@icons/stacks/MailgunIcon";
+import MaterialUIIcon from "@icons/stacks/MaterialUIIcon";
+import MySQLIcon from "@icons/stacks/MySQLIcon";
+import NestJSIcon from "@icons/stacks/NestJSIcon";
+import ReactIcon from "@icons/stacks/ReactIcon";
+import SupabaseIcon from "@icons/stacks/SupabaseIcon";
+import TailwindCSSIcon from "@icons/stacks/TailwindCSSIcon";
+import TypeScriptIcon from "@icons/stacks/TypeScriptIcon";
+import VercelIcon from "@icons/stacks/VercelIcon";
+import ViteIcon from "@icons/stacks/ViteIcon";
+import WordpressIcon from "@icons/stacks/WordpressIcon";
+
+// Tech Stacks Data
+export const TECH_STACKS = [
+  {
+    name: "AWS",
+    Icon: AWSIcon,
+  },
+  {
+    name: "BootStrap",
+    Icon: BootStrapIcon,
+  },
+  {
+    name: "CSS",
+    Icon: CSSIcon,
+  },
+  {
+    name: "Firebase",
+    Icon: FirebaseIcon,
+  },
+  {
+    name: "Github",
+    Icon: GithubIcon,
+  },
+  {
+    name: "Git",
+    Icon: GitIcon,
+  },
+  {
+    name: "HTML",
+    Icon: HTMLIcon,
+  },
+  {
+    name: "JavaScript",
+    Icon: JavaScriptIcon,
+  },
+  {
+    name: "Laravel",
+    Icon: LaravelIcon,
+  },
+  {
+    name: "Mailgun",
+    Icon: MailgunIcon,
+  },
+  {
+    name: "Material UI",
+    Icon: MaterialUIIcon,
+  },
+  {
+    name: "MySQL",
+    Icon: MySQLIcon,
+  },
+  {
+    name: "NestJS",
+    Icon: NestJSIcon,
+  },
+  {
+    name: "React",
+    Icon: ReactIcon,
+  },
+  {
+    name: "Supabase",
+    Icon: SupabaseIcon,
+  },
+  {
+    name: "TailwindCSS",
+    Icon: TailwindCSSIcon,
+  },
+  {
+    name: "TypeScript",
+    Icon: TypeScriptIcon,
+  },
+  {
+    name: "Vercel",
+    Icon: VercelIcon,
+  },
+  {
+    name: "Vite",
+    Icon: ViteIcon,
+  },
+  {
+    name: "Wordpress",
+    Icon: WordpressIcon,
+  },
+];
