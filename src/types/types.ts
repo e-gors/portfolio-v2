cb18f0e3349bae3d7b8554cc0fe48062f62d477b
egoron-dev <goronefren@gmail.com>
2025-12-14 18:11:23 +0800

    added experience and education props

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,18 @@
-export type TechStack = {
+export type TechStackProps = {
     name: string;
     Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+}
+
+export type ExperienceItemProps = {
+    title: string;
+    company?: string;
+    date: string;
+    description?: string;
+}
+
+export type EducationProps = {
+    degree: string;
+    school?: string;
+    date: string;
+    description?: string;
 }
