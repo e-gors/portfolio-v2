2030cedf4595e4032a4c20caaea283f88b790913
egoron-dev <goronefren@gmail.com>
2025-12-14 16:52:39 +0800

    done about section with tech stacks

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,55 +1,81 @@
 import React from "react";
 import AboutImage from "@images/about-image.png";
 import TitleHeader from "../components/TitleHeader";
+import GithubRoundedIcon from "@icons/GithubRoundedIcon";
+import FacebookRoundedIcon from "@icons/FacebookRoundedIcon";
+import LinkedInRoundedIcon from "@icons/LinkedInRoundedIcon";
+import DiscordRoundedIcon from "@icons/DiscordRoundedIcon";
+import GradientSphere from "../components/GradientSphere";
+import { TECH_STACKS } from "@constants/constants";
 
 export default function About() {
   return (
     <section
       id="about"
-      className="w-screen bg-primary relative mx-auto min-h-screen px-4 md:px-10 lg:px-20 py-12 gap-8 md:gap-12"
+      className="w-screen bg-primary relative mx-auto px-4 md:px-10 lg:px-20 py-12 gap-8 md:gap-12 z-1"
     >
       <TitleHeader title="About Me" number="01" />
 
       <div className="content flex flex-wrap flex-col lg:flex-row pt-10">
-        <div className="left flex-1 justify-center md:px-2">
+        <div className="left flex-1 justify-center md:px-2 pb-5 lg:pb-0">
           <img
             src={AboutImage}
             alt="About Me"
-            className="max-w-full h-auto w-auto md:w-[420px] lg:w-[520px]"
+            className="max-w-full h-auto w-auto md:w-auto lg:w-[520px]"
           />
         </div>
-        <div className="right flex-1">
+        <div className="right flex-1 space-y-4">
           <p className="text-base md:text-lg text-justify md:text-left">
-            I’m a dedicated developer who thrives on new challenges and is
-            always eager to grow. My passion for building responsive, engaging
-            web applications keeps me up to date with the latest technologies
-            and trends. I aim to use my skills to contribute to meaningful
-            projects and continue evolving as a developer. I value clean,
-            maintainable code and enjoy collaborating with others to solve
-            complex problems. I take pride in writing code that’s not just
-            functional, but scalable and easy to understand.
+            I’m a dedicated full-stack developer who thrives on new challenges
+            and continuous growth. I build responsive, engaging web applications
+            with a strong focus on clean, maintainable, and scalable code. I
+            enjoy collaborating with others to solve complex problems and take
+            pride in creating software that is both functional and easy to
+            understand.
           </p>
           <p className="text-base md:text-lg text-justify md:text-left">
-            I specialize in full-stack web development, combining front-end and
-            back-end expertise. I’ve built interactive UIs with React and Redux
-            Toolkit, using libraries like Material UI, Tailwind CSS, and
-            Bootstrap for responsive design. On the back end, I’ve developed
-            scalable APIs with NestJS and Laravel, following best practices in
-            architecture and code quality.
+            I specialize in front-end and back-end development using React,
+            Redux Toolkit, Tailwind CSS, Material UI, NestJS, and Laravel. At
+            JLabs, I worked on a real-world vehicle management platform
+            featuring bookings, auctions, real-time updates, and secure
+            transactions. I also have experience with AWS services like S3 and
+            CloudFront to improve performance and scalability, and I’m always
+            learning and refining my craft to build impactful software.
           </p>
-          <p className="text-base md:text-lg text-justify md:text-left">
-            During my time at JLabs, I worked on a real-world vehicle management
-            platform that supported bookings, auctions, sales, and trade-ins.
-            The system featured user authentication, listing and bid management,
-            real-time auction updates, and secure transaction workflows. I also
-            have experience working with AWS, particularly S3 and CloudFront, to
-            enhance performance and scalability. My workflow focuses on writing
-            clean code, improving usability, and optimizing for performance in
-            every project. I’m always learning, always building, and always
-            striving to create software that makes an impact.
-          </p>
+          <div className="flex gap-x-4 h-20 mt-5">
+            <div className="icon rounded-xl bg-white/5">
+              <FacebookRoundedIcon width={40} height={40} />
+            </div>
+            <div className="icon rounded-xl bg-white/5">
+              <GithubRoundedIcon width={60} />
+            </div>
+            <div className="icon rounded-xl bg-white/5">
+              <LinkedInRoundedIcon width={40} />
+            </div>
+            <div className="icon rounded-xl bg-white/5">
+              <DiscordRoundedIcon width={40} />
+            </div>
+          </div>
         </div>
       </div>
+
+      {/* Tech Stacks */}
+      <div className="tech-stack overflow-hidden pt-5 lg:pt-10 relative w-full">
+        <div className="stack-icon-container flex flex-nowrap gap-x-8 animate-scroll">
+          {[...TECH_STACKS, ...TECH_STACKS].map(({ name, Icon }, index) => (
+            <div
+              key={`${name}-${index}`}
+              className="stack-card stack-icon flex items-center justify-center rounded-xl bg-white/5 transition shrink-0 cursor-pointer w-18 h-18 md:w-25 md:h-25"
+              title={name}
+            >
+              <Icon />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Gradient Spheres */}
+      <GradientSphere firstSphere="sphere-3" secondSphere="sphere-4" />
     </section>
   );
 }
