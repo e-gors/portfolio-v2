91dfabfea6b6e116e751ee59be981f295e806738
egoron-dev <goronefren@gmail.com>
2025-12-14 18:10:36 +0800

    added padding bottom

--- src/components/TitleHeader.tsx
+++ src/components/TitleHeader.tsx
@@ -7,7 +7,7 @@ interface TitleHeaderProps {
 
 export default function TitleHeader(props: TitleHeaderProps) {
   return (
-    <div className="flex wrap justify-between items-center">
+    <div className="flex wrap justify-between items-center pb-5">
       <h2 className="text-xl md:text-4xl font-semibold">{props.title}</h2>
       <div className="flex items-center gap-4">
         <span className="h-px md:h-1 w-15 sm:w-30 md:w-50 bg-white/40 rounded-xs"></span>
