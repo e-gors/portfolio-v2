56b4716347f26f89eb0ac3720eb1981cd1a1ea3e
egoron-dev <goronefren@gmail.com>
2025-12-15 14:55:00 +0800

    created and finalized contact section

--- src/sections/Contacts.tsx
+++ src/sections/Contacts.tsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import React from "react";
+import EmailIcon from "@icons/EmailIcon";
+import PhoneIcon from "@icons/PhoneIcone";
+import LocationIcon from "@icons/LocationIcon";
+import GradientSphere from "@components/GradientSphere";
+import CustomButton from "@components/CustomButton";
 
 export default function Contacts() {
   return (
-    <div>Contacts</div>
-  )
+    <section
+      id="contacts"
+      className="w-screen bg-primary relative mx-auto px-4 md:px-10 lg:px-20 py-12 gap-8 md:gap-12 z-1 flex flex-wrap overflow-hidden"
+    >
+      <div className="left flex-1">
+        <h2 className="text-3xl font-bold mb-4">Get in Touch</h2>
+        <p className="text-sm mb-6">
+          Feel free to get in touch! Whether you have questions, feedback, or
+          just want to say hello, I’d be happy to hear from you. Don't hesitate
+          to reach out using the contact information provided below. I’m always
+          open to connecting and discussing ideas, so let’s chat!
+        </p>
+        <div className="flex gap-2 my-2">
+          <EmailIcon width={24} height={24} />
+          <p className="text-sm">goronefren@gmail.com</p>
+        </div>
+        <div className="flex gap-2 my-2">
+          <PhoneIcon width={24} height={24} />
+          <p className="text-sm">+63-936-141-6119</p>
+        </div>
+        <div className="flex gap-2 my-2">
+          <LocationIcon width={24} height={24} />
+          <p className="text-sm">
+            Purok Lapulapu Brgy. Atabay Hilongos, Leyte, Philippines
+          </p>
+        </div>
+      </div>
+      <div className="right flex-1">
+        <form className="flex flex-col max-w-lg">
+          <input
+            type="text"
+            placeholder="Your Name"
+            className="mb-4 p-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          />
+          <input
+            type="email"
+            placeholder="Your Email"
+            className="mb-4 p-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          />
+          <textarea
+            placeholder="Your Message"
+            className="mb-4 p-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            rows={5}
+          ></textarea>
+          <CustomButton variant="contained" className="w-[130px]">
+            Send Message
+          </CustomButton>
+        </form>
+      </div>
+
+      {/* Gradient Spheres */}
+      <GradientSphere firstSphere="sphere-1" secondSphere="sphere-4" />
+    </section>
+  );
 }
