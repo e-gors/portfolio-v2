6ac931c4d45257b22e5976d41ada3e6158d99d26
egoron-dev <goronefren@gmail.com>
2025-12-19 22:58:29 +0800

    removed button inside form

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -138,21 +138,6 @@ export default function Testimonials() {
               </div>
             </div>
           </div>
-
-          <div className="flex items-center justify-end gap-x-6">
-            <button
-              type="button"
-              className="text-sm/6 font-semibold text-white"
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-            >
-              Save Feedback
-            </button>
-          </div>
         </form>
       </BaseModal>
 
