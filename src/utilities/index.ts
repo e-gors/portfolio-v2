398a0d25ea4ff2a4b1fe79d2846411b8767fadbe
egoron-dev <goronefren@gmail.com>
2025-12-20 01:42:24 +0800

    created utils

---
+++ src/utilities/index.ts
@@ -0,0 +1,18 @@
+import supabase from "@/config/supabase-client";
+import DefaultPerson from "@images/boy.png";
+
+export const getAvatar = (path: string | null | undefined) => {
+  if (!path) return DefaultPerson;
+  if (path.startsWith("http")) return path;
+  return supabase.storage.from("portfolio").getPublicUrl(path).data.publicUrl;
+};
+
+export const convertDate = (date: string | Date) => {
+  if (!date) return "";
+
+  return new Date(date).toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+};
